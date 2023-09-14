6e8efe193553e82c9e7430e7a86d29399fb1e55a
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-09-14 20:58:52 +0200

    Reformat Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  })]
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ]
 });
