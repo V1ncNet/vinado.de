0fd97dc7185def0e4e84e32dc25191d97702b23b
Vincent Nadoll <vincent.nadoll@googlemail.com>
2024-04-13 15:46:19 +0200

    Replace deprecated package

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-import Compress from "astro-compress";
+import Compress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
