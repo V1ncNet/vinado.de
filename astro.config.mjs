46357539578063ddaef79fe177c11d001066fa2f
Vincent Nadoll <vincent.nadoll@googlemail.com>
2024-04-13 15:46:50 +0200

    Order imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import robotsTxt from "astro-robots-txt";
+import tailwind from "@astrojs/tailwind";
 import Compress from "@playform/compress";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
